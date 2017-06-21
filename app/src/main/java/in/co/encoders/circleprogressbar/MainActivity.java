f793df170412ce687b305185eea23c4a13ea7c6e
avik <rayan>
2017-06-21 13:24:08 +0530

    initcommit

---
+++ app/src/main/java/in/co/encoders/circleprogressbar/MainActivity.java
@@ -0,0 +1,69 @@
+package in.co.encoders.circleprogressbar;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the widgets reference from XML layout
+        final RelativeLayout rl = (RelativeLayout) findViewById(R.id.rl);
+        final Button btn = (Button) findViewById(R.id.btn);
+        final TextView tv = (TextView) findViewById(R.id.tv);
+        final ProgressBar pb = (ProgressBar) findViewById(R.id.pb);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Set the progress status zero on each button click
+                progressStatus = 0;
+
+                // Start the lengthy operation in a background thread
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while(progressStatus < 100){
+                            // Update the progress status
+                            progressStatus +=1;
+
+                            // Try to sleep the thread for 20 milliseconds
+                            try{
+                                Thread.sleep(20);
+                            }catch(InterruptedException e){
+                                e.printStackTrace();
+                            }
+
+                            // Update the progress bar
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    pb.setProgress(progressStatus);
+                                    // Show the progress on TextView
+                                    tv.setText(progressStatus+"");
+                                    // If task execution completed
+                                    if(progressStatus == 100){
+                                        // Set a message of completion
+                                        tv.setText("Operation completed.");
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }).start(); // Start the operation
+            }
+        });
+
+    }
+}
